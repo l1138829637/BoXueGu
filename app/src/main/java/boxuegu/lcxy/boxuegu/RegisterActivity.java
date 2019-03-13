ec0c6c2b5742558afc9bfb37803ae8f11b7533e6
lhd <1138829637@qq.com>
2019-03-13 10:53:26 +0800

    Initial commit

---
+++ app/src/main/java/boxuegu/lcxy/boxuegu/RegisterActivity.java
@@ -0,0 +1,75 @@
+package boxuegu.lcxy.boxuegu;
+
+import android.content.pm.ActivityInfo;
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class RegisterActivity extends AppCompatActivity {
+    private TextView tv_main_title;//标题
+    private TextView tv_back;      //返回按钮
+    private Button   btn_register;//注册按钮
+    //用户名、密码、再次输入密码的控件
+    private EditText et_user_name,et_psw,et_psw_again;
+    //用户名、密码、再次输入密码的控件的获取值
+    private String userName,psw,pswAgain;
+    //标题布局
+    private RelativeLayout rl_title_bar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        //设置页面布局
+        setContentView(R.layout.activity_register);
+        //设置此界面为竖屏
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        init();
+    }
+
+    private void init() {
+        //从main_title_bar.xml页面布局中获取对应的UI控件
+        tv_main_title = findViewById(R.id.tv_main_title);
+        tv_main_title.setText("注册");
+        tv_back = findViewById(R.id.tv_back);
+        rl_title_bar = findViewById(R.id.title_bar);
+        rl_title_bar.setBackgroundColor(Color.TRANSPARENT);
+        //从activity_register.xml页面布局中获取对应的UI控件
+        btn_register = findViewById(R.id.btn_register);
+        et_user_name = findViewById(R.id.et_user_name);
+        et_psw = findViewById(R.id.et_psw);
+        et_psw_again = findViewById(R.id.et_psw_again);
+        tv_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                RegisterActivity.this.finish();
+            }
+        });
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //获取输入在相应控件中的字符串
+                getEditString();
+                if (TextUtils.isEmpty(userName)) {
+                    Toast.makeText(RegisterActivity.this, "请输入用户名", Toast.LENGTH_SHORT).show();
+                    return;
+                } else if (TextUtils.isEmpty(psw)) {
+                    Toast.makeText(RegisterActivity.this, "请输入密码", Toast.LENGTH_SHORT).show();
+                    return;
+                } else if (TextUtils.isEmpty(pswAgain)) {
+                    Toast.makeText(RegisterActivity.this, "请再次输入密码", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+            }
+        });
+    }
+
+    private void getEditString() {
+    }
+}
